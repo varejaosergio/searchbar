90e118914bf6378918548299dc497399ae35021b
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-01-04 11:43:45 -0800

    onVideoSelect start implementation

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,15 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = (props) => {
-    return <div>Video item</div>;
+const VideoItem = ({ video, onVideoSelect }) => {
+    return ( 
+        <div onClick={() => onVideoSelect (video)} className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="" />
+            <div className="content">
+            <div className="header">{video.snippet.title}</div>
+            </div>
+        </div>
+    );
 };
 
 export default VideoItem;
