34e0e6b686006ec462bb9bb214d6d3dcde2ec079
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-01-06 11:40:54 -0800

    styles and semantic ui changes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,11 @@ import React from 'react';
 const VideoItem = ({ video, onVideoSelect }) => {
     return ( 
         <div onClick={() => onVideoSelect (video)} className="video-item item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="" />
+            <img 
+                className="ui image" 
+                src={video.snippet.thumbnails.medium.url} 
+                alt={video.snippet.title} 
+            />
             <div className="content">
             <div className="header">{video.snippet.title}</div>
             </div>
